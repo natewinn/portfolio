21cd242096b9609afca7b3f5063e4e8a540100d9
Nate Winn <nathan.winn@gmail.com>
2014-07-28 06:24:51 -0700

    css tweaks and image optimization

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,38 +1,20 @@
 $(function () {
+  
+  // Mobile Navigation Toggle //
   document.querySelector( "#nav-toggle" )
   .addEventListener( "click", function() {
     this.classList.toggle( "active" );
   });
 
+  // Carousel Speed //
   $('.carousel').carousel({
 	interval: 8000
 	});
 
-  if (Modernizr.touch) {
-      // show the close overlay button
-      $(".close-overlay").removeClass("hidden");
-      // handle the adding of hover class when clicked
-      $(".img").click(function(e){
-          if (!$(this).hasClass("hover")) {
-              $(this).addClass("hover");
-          }
-      });
-      // handle the closing of the overlay
-      $(".close-overlay").click(function(e){
-          e.preventDefault();
-          e.stopPropagation();
-          if ($(this).closest(".img").hasClass("hover")) {
-              $(this).closest(".img").removeClass("hover");
-          }
-      });
-  } else {
-      // handle the mouseenter functionality
-      $(".img").mouseenter(function(){
-          $(this).addClass("hover");
-      })
-      // handle the mouseleave functionality
-      .mouseleave(function(){
-          $(this).removeClass("hover");
-      });
-  }
+  // Make "Thumb Div" Clickable // 
+  $(".thumb").click(function(){
+       window.location=$(this).find("a").attr("href");
+       return false;
+  });
+
 });
--- style_guide.md
+++ style_guide.md
@@ -1,11 +1,8 @@
 #Style Guide
 
-Primay (Dark) Gray - #434A4F
-Secondary (Light) Gray - #F1F2F2
-Accent color - #FFA500
-Anchor tag color - #FFA500
-Line - #CCC
-
-
-
-
+1. Primay (Dark) Gray - #3A4044
+2. Primary Text Color - #3A4044
+3. Secondary (Light) Gray - #F1F2F2
+4. Accent color - #FFA500
+5. Anchor tag hover - #FFA500
+6. Line - #CCC
